6b608d327b6f8cc0345b9d4d957245eadad6b583
GpapPeaky <giorgospapamat@gmail.com>
2025-10-20 20:27:00 +0300

    Initial commit

---
+++ src/SDL2_Event.hpp
@@ -0,0 +1,10 @@
+#include "../ThirdParty/SDL2/include/SDL2/SDL.h"
+
+#pragma once
+
+/**
+ * @brief Handles main loop events
+ * 
+ * @param quit Checks if we have quit the main loop
+ */
+void SDL2_HandleEvents(bool& quit);
---
+++ src/SDL2_InitWin.hpp
@@ -0,0 +1,16 @@
+#include "../ThirdParty/SDL2/include/SDL2/SDL.h"
+#include "../ThirdParty/SDL2/include/SDL2/SDL_image.h"
+#include <iostream>
+
+#pragma once
+
+#define SUCCESS 0
+#define FAILURE -1
+
+extern SDL_Window* SDL2_Win;
+extern SDL_Renderer* SDL2_Rnd;
+
+/**
+ * @brief Initialises the renderer, the window, and the SDL/IMG modes required
+*/
+int SDL2_InitWin(void);
